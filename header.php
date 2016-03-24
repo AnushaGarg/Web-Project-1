a53ea3ef7ad2afaa7ff43a3b0e21313a5be9eace
APnatural <Abhiroj95@gmail.com>
2016-03-24 19:44:40 +0530

    Add destination content

--- header.php
+++ header.php
@@ -1,32 +1,154 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="Styler.css"/>
-  <title>Bootstrap Case</title>
+  <title>HillyHolidays</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="Styler.css"/>
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse " >
   <div class="container-fluid">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
+    <ul class="nav navbar-nav ">
+        <li class="active"><a href="#" class="nav">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Hotels <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Hotels-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
+          <li><a href="#" >Hotels-1</a></li>
+          <li><a href="#" >Page 1-2</a></li>
+          <li><a href="#" >Page 1-3</a></li>
         </ul>
       </li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Destinations <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Destinations-1</a></li>
-          <li><a href="#">Destinations-2</a></li>
-          <li><a href="#">Destinations-3</a></li>
+      <li class="dropdown"><a class="dropdown-toggle nav1" data-toggle="dropdown" href="#">Destinations <span class="caret"></span></a>
+        <ul class="dropdown-menu"> 
+            <div class="table-responsive">
+            <table class="table">
+                <tbody>
+                <tr>
+          <td><li><a href="#">Must Visit</a></li></td>
+            <td><li><a href="#">Chaukori</a></li></td>
+            <td><li><a href="#">Jeolikot</a></td>
+            <td><li><a href="#">Mori</a></td>
+                </tr>
+      <tr>
+         <td><li><a href="#">Popular Destination</a></li></td>
+            <td><li><a href="#">Rishikesh</a></li></td>
+            <td><li><a href="#">Kailash Mansarovar</a></td>
+            <td><li><a href="#">Piran Kaliyar Sharif</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Almora</a></li></td>
+            <td><li><a href="#">Devalgarh</a></li></td>
+            <td><li><a href="#">Bhimtal</a></td>
+            <td><li><a href="#">Munsyuari</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Auli</a></li></td>
+            <td><li><a href="#">Chopta</a></li></td>
+            <td><li><a href="#">Leti</a></td>
+            <td><li><a href="#">Rudraprayag</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Corbett Nationa Park</a></li></td>
+            <td><li><a href="#">Jilling</a></li></td>
+            <td><li><a href="#">Kathgodam</a></td>
+            <td><li><a href="#">Uttarkashi</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Badrinath</a></li></td>
+            <td><li><a href="#">Dhanaulti</a></li></td>
+            <td><li><a href="#">Gopeshwar</a></td>
+            <td><li><a href="#">Pangot</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Bageshwar</a></li></td>
+            <td><li><a href="#">Gwaldam</a></li></td>
+            <td><li><a href="#">Kedarnath</a></td>
+            <td><li><a href="#">Nanakmatta</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Dehradun</a></li></td>
+            <td><li><a href="#">Abott Mount</a></li></td>
+            <td><li><a href="#">Kanatal</a></td>
+            <td><li><a href="#">Valley of Flowers</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Binsar</a></li></td>
+            <td><li><a href="#">Kalpeshwar</a></li></td>
+            <td><li><a href="#">Lansdowne</a></td>
+            <td><li><a href="#">Naukuchiatal</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Haridwar</a></li></td>
+            <td><li><a href="#">Devprayag</a></li></td>
+            <td><li><a href="#">Mallital</a></td>
+            <td><li><a href="#">Yamunotri</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Bhowali</a></li></td>
+            <td><li><a href="#">Barkot</a></li></td>
+            <td><li><a href="#">Kausani</a></td>
+            <td><li><a href="#">Patal Bhuvaneshwar</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Panch Badri</a></li></td>
+            <td><li><a href="#">Kalsi</a></li></td>
+            <td><li><a href="#">Mukteshwar</a></td>
+            <td><li><a href="#">Pauri</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Mussorie</a></li></td>
+            <td><li><a href="#">Gangotri</a></li></td>
+            <td><li><a href="#">Khirsu</a></td>
+            <td><li><a href="#">Pithoragarh</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Chakrata</a></li></td>
+            <td><li><a href="#">Dharchula</a></li></td>
+            <td><li><a href="#">Rajaji National Park</a></td>
+            <td><li><a href="#">Ramgarh</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Hanuman Chatti</a></li></td>
+            <td><li><a href="#">Sri Hemkund Sahib</a></li></td>
+            <td><li><a href="#">Lohaghat</a></td>
+            <td><li><a href="#">Rudranath</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Chamba</a></li></td>
+            <td><li><a href="#">Guptkashi</a></li></td>
+            <td><li><a href="#">Panch Kedar</a></td>
+            <td><li><a href="#">Rudrapur</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Naintal</a></li></td>
+            <td><li><a href="#">Kanvashram</a></li></td>
+            <td><li><a href="#">Madhyamaheshwar</a></td>
+            <td><li><a href="#">Sattal</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Ranikhet</a></li></td>
+            <td><li><a href="#">Jageshwar</a></li></td>
+            <td><li><a href="#">Panch Prayag</a></td>
+            <td><li><a href="#">Tons Valley</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Champawat</a></li></td>
+            <td><li><a href="#">Harsil</a></li></td>
+            <td><li><a href="#">Mana</a></td>
+            <td><li><a href="#">Tehri-Garhwal</a></td>
+      </tr>
+      <tr>
+         <td><li><a href="#">Janki Chatti</a></li></td>
+            <td><li><a href="#">Joshimath</a></li></td>
+            <td><li><a href="#">Tunganath</a></td>
+      </tr>
+    </tbody>
+  </table>
+            </div>
         </ul>
       </li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Packages<span class="caret"></span></a>
